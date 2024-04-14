2c7f13f1a83fb63348b313241ed510aa2f7cb836
majdAlmotaem <majdalmotaem1998@gmail.com>
2024-04-14 14:27:37 +0200

    Datei erstellt für die ganze Imports

--- app_gui.py
+++ app_gui.py
@@ -1,18 +1,4 @@
-import json
-import Tools
-from kivy.lang import Builder
-from kivymd.app import MDApp
-from kivy.uix.image import Image
-from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.widget import Widget
-from kivy.uix.popup import Popup
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ListProperty
-from kivy.properties import ObjectProperty
-from kivy.uix.spinner import SpinnerOption
-from datetime import datetime
-from kivy.core.window import Window
+from imports import *
 
 
 
